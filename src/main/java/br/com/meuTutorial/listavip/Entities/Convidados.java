0a3e7b87b702e9defefbd1bda4fa9bbfa0703d14
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2022-02-11 11:04:33 -0300

    Parte 4 Adicionar dados ao BD pela pagina HTML

--- src/main/java/br/com/meuTutorial/listavip/Entities/Convidados.java
+++ src/main/java/br/com/meuTutorial/listavip/Entities/Convidados.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,5 +23,11 @@ public class Convidados {
     private String nome;
     private String email;
     private String telefone;
-      
+
+    public Convidados(String nome, String email, String telefone) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
 }
--- src/main/java/br/com/meuTutorial/listavip/controllers/CoinvidadoController.java
+++ src/main/java/br/com/meuTutorial/listavip/controllers/CoinvidadoController.java
@@ -1,13 +1,14 @@
 package br.com.meuTutorial.listavip.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.meuTutorial.listavip.Entities.Convidados;
 import br.com.meuTutorial.listavip.repository.ConvidadoRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 
 
 @Controller
@@ -28,5 +29,21 @@ public class CoinvidadoController {
 		model.addAttribute("convidados", convidados);		
 		return "listaconvidados";
 	}
+
+    @PostMapping(value = "salvar")
+	public String salvar(@RequestParam("nome") String nome, @RequestParam("email")String email, 
+			@RequestParam("telefone") String telefone, Model model){
+		
+		Convidados novoConvidado = new Convidados(nome, email, telefone);
+		
+		repository.save(novoConvidado);
+		
+		Iterable<Convidados> convidados = repository.findAll();
+		
+		
+		model.addAttribute("convidados", convidados);
+		
+		return "listaconvidados";
+        }
     
 }
