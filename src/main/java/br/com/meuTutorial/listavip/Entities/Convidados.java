d51221393814bb59478c8fb32441cb68bae93172
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2022-02-11 00:08:26 -0300

    Parte 1 BD

---
+++ src/main/java/br/com/meuTutorial/listavip/Entities/Convidados.java
@@ -0,0 +1,25 @@
+package br.com.meuTutorial.listavip.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Convidados {
+    
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+      
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Convidados (nome, email, telefone) VALUES
+('Antonio', 'antonio@email.com', '9999-1111'),
+('Bruno', 'bruno@email.com', '9999-2222'),
+('Carlos', 'carlos@email.com', '9999-3333');
