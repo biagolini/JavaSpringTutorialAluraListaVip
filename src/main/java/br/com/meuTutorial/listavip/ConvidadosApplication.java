a2a53e56d1772a1026d54fd4b886561ed2d1ba90
Carlos Biagolini-Jr <c.biagolini@gmail.com>
2022-02-11 00:06:12 -0300

    Base

---
+++ src/main/java/br/com/meuTutorial/listavip/ConvidadosApplication.java
@@ -0,0 +1,13 @@
+package br.com.meuTutorial.listavip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvidadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvidadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meuTutorial/listavip/ConvidadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meuTutorial.listavip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvidadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
